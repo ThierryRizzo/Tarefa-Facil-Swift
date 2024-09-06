d6b3553c9e03c8051b75ffa88f4b621b8a3face2
Thierry_Rizzo <thierryrizzofranco@gmail.com>
2024-09-06 18:20:29 -0300

    Initial Commit

---
+++ Tarefa facil/Tarefa_facilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tarefa_facilApp.swift
+//  Tarefa facil
+//
+//  Created by Thierry Rizzo on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Tarefa_facilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
